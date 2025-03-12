25a52767f95f3448757d318d1fed5c02461a2637
Savannah-Lu <savannahlut@nycstudents.net>
2025-03-12 13:31:04 +0000

    started

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
 public class Main{
     public static void main(String[] args){
         MemberInfo jane = new MemberInfo("SMITH, JANE", 2019, false);
-        MemberInfo steve = new MemberInfo("SMITH, JANE", 2019, false);
-        MemberInfo michael = new MemberInfo("SMITH, JANE", 2019, false);
-        MemberInfo maria = new MemberInfo("SMITH, JANE", 2019, false);
+        MemberInfo steve = new MemberInfo("FOX, STEVE", 2018, true);
+        MemberInfo michael = new MemberInfo("XIN, MICHAEL", 2019, false);
+        MemberInfo maria = new MemberInfo("GARCIA, MARIA", 2019, true);
 
+        ClubMembers PoetryClub = new ClubMembers();
     }
 }
